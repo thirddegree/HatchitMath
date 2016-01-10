e3a9ea0d5932f17caba01127d252ecc15ffdc4ab
Honeybunch <amt3824@g.rit.edu>
2016-01-10 17:02:59 -0500

    Added an inversion test for Matrix3

--- test/Matrix3Test.cpp
+++ test/Matrix3Test.cpp
@@ -13,3 +13,19 @@ TEST(Matrix3, GetAsArray)
     for(int i = 0; i < 9; i++)
       ASSERT_EQ(array[i], i+1);
 }
+
+TEST(Matrix3, Inversion)
+{
+  Matrix3 testMat(1,2,3,3,2,1,2,1,3);
+  Matrix3 inversion = testMat.getInverse();
+
+  float* result = (inversion * testMat).getAsArray();
+  float* identity = Matrix3().getAsArray();
+
+  // A matrix multiplied by its inverse should be an identity matrix
+  for(int i = 0; i < 9; i++)
+  {
+    // Using ASSERT_NEAR because floating point values aren't exact
+    ASSERT_NEAR(result[i], identity[i], 0.0000001f);
+  }
+}
