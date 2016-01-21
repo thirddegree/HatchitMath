e58b7180da393d4c8277789850ac773c1af182dc
Honeybunch <amt3824@g.rit.edu>
2016-01-21 11:07:00 -0500

    Added tests for the Ray class

--- test/RayTest.cpp
+++ test/RayTest.cpp
@@ -11,3 +11,77 @@
 **    will be met: https://www.gnu.org/licenses/lgpl.html
 **
 **/
+
+#include <gtest/gtest.h>
+#include "ht_ray.h"
+
+using namespace Hatchit;
+using namespace Math;
+
+TEST(Ray, DirectionConstructor)
+{
+  //Ray at 0,0,0 with direction 1,2,3
+  Ray ray (Vector3(1,2,3));
+
+  Vector3 origin = ray.getOrigin();
+  Vector3 direction = ray.getDirection();
+
+  //Origin should stay 0,0,0
+  ASSERT_EQ(origin[0], 0);
+  ASSERT_EQ(origin[1], 0);
+  ASSERT_EQ(origin[2], 0);
+
+  //Direction should stay 1,2,3
+  ASSERT_EQ(direction[0], 1);
+  ASSERT_EQ(direction[1], 2);
+  ASSERT_EQ(direction[2], 3);
+}
+
+TEST(Ray, OriginDirectionConstructor)
+{
+  //Ray at 5,6,7 with direction 1,2,3
+  Ray ray (Vector3(5,6,7),Vector3(1,2,3));
+
+  Vector3 origin = ray.getOrigin();
+  Vector3 direction = ray.getDirection();
+
+  //Origin should stay 5,6,7
+  ASSERT_EQ(origin[0], 5);
+  ASSERT_EQ(origin[1], 6);
+  ASSERT_EQ(origin[2], 7);
+
+  //Direction should stay 1,2,3
+  ASSERT_EQ(direction[0], 1);
+  ASSERT_EQ(direction[1], 2);
+  ASSERT_EQ(direction[2], 3);
+}
+
+TEST(Ray, SettingOrigin)
+{
+  Ray ray(Vector3(1,2,3));
+
+  //Change origin to 5,6,7
+  ray.setOrigin(Vector3(5,6,7));
+
+  Vector3 origin = ray.getOrigin();
+
+  //Origin should now be 5,6,7
+  ASSERT_EQ(origin[0], 5);
+  ASSERT_EQ(origin[1], 6);
+  ASSERT_EQ(origin[2], 7);
+}
+
+TEST(Ray, SettingDirection)
+{
+  Ray ray(Vector3(1,2,3));
+
+  //Change direction to 5,6,7
+  ray.setDirection(Vector3(5,6,7));
+
+  Vector3 direction = ray.getDirection();
+
+  //Origin should now be 5,6,7
+  ASSERT_EQ(direction[0], 5);
+  ASSERT_EQ(direction[1], 6);
+  ASSERT_EQ(direction[2], 7);
+}
