61999900f58071e8618abc995b62e5991ac69283
MattGuerrette <direct3Dtutorials@gmail.com>
2016-02-05 03:44:07 -0500

    hopefully fixed aligned_alloc issue

--- include/ht_platform.h
+++ include/ht_platform.h
@@ -97,7 +97,7 @@
     #define ALIGN_FREE(x) _aligned_free(x)
     #endif
 #else
-#include <cstdlib>
+#include <stdlib.h>
     #ifndef ALIGN_ALLOC
     #define ALIGN_ALLOC(x,y) aligned_alloc(x,y)
     #endif
